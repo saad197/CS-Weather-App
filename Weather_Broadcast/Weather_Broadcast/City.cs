b717f0134b506f36f48f5e66c2dbbee7ffbbc2e3
Karamullah Agha <k.agha809@mybvc.ca>
2018-10-03 12:23:18 -0600

    Not implemented city class

---
+++ Weather_Broadcast/Weather_Broadcast/City.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather_Broadcast
+{
+   public class City
+    {
+        public string _name { get; set; }
+        public int _id { get; set; }
+
+
+        public City(string name)
+        {
+            _name = name;
+        }
+
+        public int GetCityFromCityName()
+        {
+          throw new NotImplementedException();
+        }
+    }
+}
