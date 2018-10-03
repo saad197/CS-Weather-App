0bcbedca7629be8ceb89f6cb22ba6c7e00aff704
k.smagh938 <k.smagh938@mybvc.ca>
2018-10-03 12:13:09 -0600

    Weather Class without Implementation

---
+++ Weather_Broadcast/Weather_Broadcast/Weather.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class Weather
+{
+	public Weather()
+	{
+
+    }
+
+    public static void DisplayWeather()
+    {
+
+    }
+
+    public static void DisplayTemp()
+    {
+
+    }
+
+    public static void DisplayBackground()
+    {
+
+    }
+
+    public static void DisplayHumidity()
+    {
+
+    }
+
+    public static void DisplayRainChance()
+    {
+
+    }
+
+    public static void DisplayDayNight()
+    {
+
+    }
+
+    public static void DisplayWindSpeed()
+    {
+
+    }
+
+    public static void DisplayIcon()
+    {
+
+    }
+}
