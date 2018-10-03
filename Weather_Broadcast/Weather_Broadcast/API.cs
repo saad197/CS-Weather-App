f1e8af60b9b8e140ddf599628fad833bbc5febb2
k.smagh938 <k.smagh938@mybvc.ca>
2018-10-03 12:21:51 -0600

    Api class (without Implementation)

---
+++ Weather_Broadcast/Weather_Broadcast/API.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather_Broadcast
+{
+    class API
+    {
+        public static void GetTemp()
+        {
+
+        }
+
+        public static void GetHumidity()
+        {
+
+        }
+
+        public static void GetRainChance()
+        {
+
+        }
+
+        public static void GetWeatherCondition()
+        {
+
+        }
+
+        public static void GetWindSpeed()
+        {
+
+        }
+
+        public static void GetDailyWeather()
+        {
+
+        }
+
+        public static void GetCurrentDayTime()
+        {
+
+        }
+    }
+}
