d82e2a0c5f667d89b8e7d9d22ad863b03c6ec0d1
k.smagh938 <k.smagh938@mybvc.ca>
2018-10-10 12:34:09 -0600

    Added coversion methods to helper class

--- Weather_Broadcast/Weather_Broadcast/Helper.cs
+++ Weather_Broadcast/Weather_Broadcast/Helper.cs
@@ -8,5 +8,16 @@ namespace Weather_Broadcast
 {
     class Helper
     {
+        public string DegreeConversion { get; private set; }
+
+        public string ConvertFahrenheitToCelsius(double temp)
+        {
+            return DegreeConversion = ((5 / 9) * (temp - 32)).ToString();
+        }
+
+        public string ConvertCelsiusToFahrenheit(double temp)
+        {
+            return DegreeConversion = ((temp * 1.8) + 32).ToString();
+        }
     }
 }
