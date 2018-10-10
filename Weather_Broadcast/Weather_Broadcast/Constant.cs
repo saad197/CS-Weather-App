a1ffb4d6184abfae76e7751dd9bef7643ea143a0
hai_do_488 <h.do488@mybvc.ca>
2018-10-10 11:43:10 -0600

    Adding map city name and city id to Constant class

--- Weather_Broadcast/Weather_Broadcast/Constant.cs
+++ Weather_Broadcast/Weather_Broadcast/Constant.cs
@@ -8,5 +8,19 @@ namespace Weather_Broadcast
 {
     class Constant
     {
+        // this dictionary holds cityname and city id in pair
+        public static Dictionary<string, int> MapOfCityNameAndID = new Dictionary<string, int>()
+        {
+            { "Calgary", 5913490 },
+            { "Toronto", 6087824 },
+            { "Vancouver", 6090785 },
+            { "Saskatoon", 6141256 },
+            { "Quebec", 6325494 },
+            { "Airdrie", 5882799 },
+            { "Medicine Hat", 6071618},
+            { "Banff", 5892532 },
+            { "California", 4350049 },
+            { "Florida", 3851244 }
+        };
     }
 }
