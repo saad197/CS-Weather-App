c63bbd36f000b16d88f3abb8aec8ebfecb85d1b5
Karamullah Agha <k.agha809@mybvc.ca>
2018-10-24 13:27:05 -0600

    retreive json data from api, and parse data of the json using Newtonsoft json library.

--- Weather_Broadcast/Weather_Broadcast/API.cs
+++ Weather_Broadcast/Weather_Broadcast/API.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
 
 namespace Weather_Broadcast
 {
     public class API
     {
         City selectedCity;
+        private static dynamic jsonData { get; set; }
+        private static dynamic ParsedData { get; set; }
 
         public API()
         {
@@ -18,8 +18,42 @@ namespace Weather_Broadcast
         public API( City city)
         {
             selectedCity = city;
+
+            //fetch api data and store json
+            FetchApiData(city.Name);
+            ParsedData = ParseData(jsonData);
         }
 
+
+
+
+        //used to make api call and retrieve json
+        private async static void FetchApiData(string city)
+        {
+            //appid is the api key
+            string apiLink = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=dd9231cf046811167ae556241662dc09";
+
+            // ... Use HttpClient.
+            using (HttpClient client = new HttpClient())
+            using (HttpResponseMessage response = await client.GetAsync(apiLink))
+            using (HttpContent content = response.Content)
+
+            {
+                // ... Read the string.
+                string result = await content.ReadAsStringAsync();
+                jsonData = result;
+            }
+        }
+
+        //will parse data
+        private dynamic ParseData(dynamic jsondata)
+        {
+            dynamic result = JsonConvert.DeserializeObject<dynamic>(jsondata);
+            return result;
+        }
+
+        
+        
         public double GetTemp()
         {
             return 0.0;
