a74ff05abbacdc2f8324cc6956f4be998767f951
Karamullah Agha <k.agha809@mybvc.ca>
2018-11-29 02:19:00 -0700

    UI changes

--- Weather_Broadcast/Weather_Broadcast/WeatherBoard.Designer.cs
+++ Weather_Broadcast/Weather_Broadcast/WeatherBoard.Designer.cs
@@ -28,34 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.picBoxBgImageWelcomePage = new System.Windows.Forms.PictureBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WeatherBoard));
             this.logo = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.btnConfirm = new System.Windows.Forms.Button();
             this.selectCityTextBox = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.picBoxBgImageWelcomePage)).BeginInit();
+            this.Refresh = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // picBoxBgImageWelcomePage
-            // 
-            this.picBoxBgImageWelcomePage.Image = global::Weather_Broadcast.Properties.Resources.kumiko_shimizu_731627_unsplash;
-            this.picBoxBgImageWelcomePage.Location = new System.Drawing.Point(16, 8);
-            this.picBoxBgImageWelcomePage.Margin = new System.Windows.Forms.Padding(2);
-            this.picBoxBgImageWelcomePage.Name = "picBoxBgImageWelcomePage";
-            this.picBoxBgImageWelcomePage.Size = new System.Drawing.Size(805, 550);
-            this.picBoxBgImageWelcomePage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.picBoxBgImageWelcomePage.TabIndex = 0;
-            this.picBoxBgImageWelcomePage.TabStop = false;
             // 
             // logo
             // 
-            this.logo.Location = new System.Drawing.Point(332, 47);
+            this.logo.BackColor = System.Drawing.Color.Transparent;
+            this.logo.Image = ((System.Drawing.Image)(resources.GetObject("logo.Image")));
+            this.logo.Location = new System.Drawing.Point(14, 0);
             this.logo.Margin = new System.Windows.Forms.Padding(2);
             this.logo.Name = "logo";
-            this.logo.Size = new System.Drawing.Size(145, 114);
-            this.logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.logo.Size = new System.Drawing.Size(32, 37);
+            this.logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.logo.TabIndex = 1;
             this.logo.TabStop = false;
             // 
@@ -64,71 +63,173 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(299, 191);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(279, 170);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(198, 35);
+            this.label1.Size = new System.Drawing.Size(230, 29);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Weather App";
+            this.label1.Text = "Weather Broadcast";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(273, 304);
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(324, 239);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(261, 35);
+            this.label2.Size = new System.Drawing.Size(141, 29);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Choose your City";
+            this.label2.Text = "Enter your city:";
             // 
             // btnConfirm
             // 
-            this.btnConfirm.Location = new System.Drawing.Point(314, 449);
+            this.btnConfirm.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnConfirm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConfirm.Location = new System.Drawing.Point(312, 349);
             this.btnConfirm.Margin = new System.Windows.Forms.Padding(2);
             this.btnConfirm.Name = "btnConfirm";
-            this.btnConfirm.Size = new System.Drawing.Size(163, 43);
+            this.btnConfirm.Size = new System.Drawing.Size(162, 35);
             this.btnConfirm.TabIndex = 5;
             this.btnConfirm.Text = "Confirm";
-            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.UseVisualStyleBackColor = false;
             this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
             // 
             // selectCityTextBox
             // 
-            this.selectCityTextBox.Location = new System.Drawing.Point(319, 374);
+            this.selectCityTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectCityTextBox.Location = new System.Drawing.Point(236, 281);
+            this.selectCityTextBox.Margin = new System.Windows.Forms.Padding(2);
             this.selectCityTextBox.Name = "selectCityTextBox";
-            this.selectCityTextBox.Size = new System.Drawing.Size(150, 22);
+            this.selectCityTextBox.Size = new System.Drawing.Size(300, 44);
             this.selectCityTextBox.TabIndex = 6;
+            this.selectCityTextBox.Text = "Calgary";
+            // 
+            // Refresh
+            // 
+            this.Refresh.BackColor = System.Drawing.Color.DarkRed;
+            this.Refresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Refresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Refresh.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Refresh.Location = new System.Drawing.Point(682, 1);
+            this.Refresh.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Refresh.Name = "Refresh";
+            this.Refresh.Size = new System.Drawing.Size(53, 36);
+            this.Refresh.TabIndex = 7;
+            this.Refresh.Text = "x";
+            this.Refresh.UseVisualStyleBackColor = false;
+            this.Refresh.Click += new System.EventHandler(this.Refresh_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.Refresh);
+            this.panel1.Controls.Add(this.logo);
+            this.panel1.Location = new System.Drawing.Point(-4, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(819, 37);
+            this.panel1.TabIndex = 8;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(50, 5);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(230, 29);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Weather Broadcast";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(337, 84);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(87, 72);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(282, 236);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(44, 36);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(624, 1);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 36);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "_";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // WeatherBoard
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(836, 582);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(731, 470);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.selectCityTextBox);
             this.Controls.Add(this.btnConfirm);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.logo);
-            this.Controls.Add(this.picBoxBgImageWelcomePage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
             this.Name = "WeatherBoard";
             this.Text = "Weather Broadcast";
-            ((System.ComponentModel.ISupportInitialize)(this.picBoxBgImageWelcomePage)).EndInit();
+            this.Load += new System.EventHandler(this.WeatherBoard_Load);
             ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox picBoxBgImageWelcomePage;
         private System.Windows.Forms.PictureBox logo;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnConfirm;
         private System.Windows.Forms.TextBox selectCityTextBox;
+        private System.Windows.Forms.Button Refresh;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Weather_Broadcast/Weather_Broadcast/WeatherBoard.cs
+++ Weather_Broadcast/Weather_Broadcast/WeatherBoard.cs
@@ -13,12 +13,18 @@ namespace Weather_Broadcast
     public partial class WeatherBoard : Form
     {
         List<City> cityList;
-        
+
+
+        //form movement variables
+        private bool isDragging = false;
+        private Point newPoint = new Point(0, 0);
+
         public WeatherBoard()
         {
             InitializeComponent();
             FillListCityData();
-            FillCityNamesToCityComboBox();          
+            FillCityNamesToCityComboBox();
+
         }
 
         private void FillListCityData()
@@ -53,6 +59,45 @@ namespace Weather_Broadcast
             API apiWeather = new API(currentSelectedCityName);
             //fetch api to get current weather data and store them in CurrentWeatherResponseFromAPI field
             API.FetchWeatherDataFromAPI();
+        }
+
+        private void WeatherBoard_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Refresh_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        //moves window around based on mouse movement
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                Point p = PointToScreen(e.Location);
+                Location = new Point(p.X - newPoint.X, p.Y - newPoint.Y);
+            }
+        }
+
+        //when mouse is down on panel
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDragging = true;  
+            newPoint = new Point(e.X, e.Y);
+
+        }
+
+        //when user lets go of mouse
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
+        }
+
+        //minimize
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
         }
     }
 }
